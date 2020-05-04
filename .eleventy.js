33fd58c984b937bd99baf7dce6143eaf40d5dd3b
james foran <james_foran@me.com>
2020-05-04 21:46:24 +1000

    updating reference view

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function(config) {
   // Passthrough copy
   config.addPassthroughCopy('src/fonts');
   config.addPassthroughCopy('src/images');
+  config.addPassthroughCopy({"src/_includes/assets/css": "css"},);
   config.addPassthroughCopy('src/js');
   config.addPassthroughCopy('src/admin/config.yml');
   config.addPassthroughCopy('src/admin/previews.js');
